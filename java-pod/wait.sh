9778a09c7c3ec5fa965f70d711ee9af92f9fb79d
Hypercube <8677100+hypercube-software@users.noreply.github.com>
2026-09-10 22:58:00 +0200

    wait.sh

--- java-pod/wait.sh
+++ java-pod/wait.sh
@@ -3,7 +3,7 @@
 cat << 'EOF' > /tmp/wait.sh
 #!/bin/bash
 clear
-echo "⏳ Initialisation de l'environnement (JDK, K9S, compilation maven etc..), veuillez patienter..."
+echo "⏳ Initialisation de l'environnement (JDK, K9S, compilation maven etc..), veuillez patienter 2 minutes..."
 
 # Enregistrement du temps de départ
 START_TIME=$SECONDS
@@ -12,20 +12,18 @@ while [ ! -f ~/killercoda-training/microservice-1/target/demo-0.0.1-SNAPSHOT.jar
     sleep 1
 done
 
-# Calcul de la durée écoulée
 DURATION=$((SECONDS - START_TIME))
 
-# Conversion en minutes et secondes
 MINUTES=$((DURATION / 60))
 SECONDS_LEFT=$((DURATION % 60))
 
-# Formatage MM:SS avec rajout du zéro initial si < 10
 ELAPSED=$(printf "%02d:%02d" $MINUTES $SECONDS_LEFT)
 
 clear
 echo "✅ Environnement prêt ! Vous pouvez commencer le TP."
-echo "⏱️  Temps d'initialisation : ${ELAPSED} (mm:ss)"
+echo "⏱️  Temps d'initialisation : ${ELAPSED}"
 EOF
 
 chmod +x /tmp/wait.sh
-/tmp/wait.sh
+source /tmp/wait.sh
+source ~/.bashrc
